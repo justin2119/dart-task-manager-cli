3dfaa3e4cd86c2258068b96f3222d88674e0f7ec
Justin.dev <justinbina515@gmail.com>
2026-07-09 16:08:03 +0000

    docs: add doc comments to exceptions to satisfy analysis_options.yaml

--- lib/exceptions/task_exceptions.dart
+++ lib/exceptions/task_exceptions.dart
@@ -1,12 +1,18 @@
+/// Exception thrown when a task is not found.
 class TaskNotFoundException implements Exception {
+  /// The ID of the task that was not found.
   final String id;
+  /// Creates a [TaskNotFoundException].
   TaskNotFoundException(this.id);
   @override
   String toString() => 'Task with ID $id not found.';
 }
 
+/// Exception thrown for storage-related errors.
 class StorageException implements Exception {
+  /// The error message.
   final String message;
+  /// Creates a [StorageException].
   StorageException(this.message);
   @override
   String toString() => 'Storage Error: $message';
